a4b789465ceb07bd10c42622516291152bae723e
Natali <TimoshenkoNatali@mail.ru>
2021-11-18 15:16:28 +0400

    send e-mail

--- index.php
+++ index.php
@@ -9,15 +9,22 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/send.php';
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MailTo</title>
-  <link rel="stylesheet" href="..css/normalize.css">
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
-  <form action="" method="post">
-    <input type="text" name="fio" placeholder="ФИО">
-    <input type="text" name="to" placeholder="Е-майл">
-    <input type="text" name="theme" placeholder="Тема сообщения">
-    <input type="textarea" name="text" placeholder="Текст сообщения">
-    <input type="submit" name="send" text="Отправить">
+<body class="flex">
+  <div class="container flex">
+  <h2 class="title">
+    Отправка е-майл сообщения
+  </h2>
+  <form action="" method="post" class="flex form">
+    <input type="text" name="fio" placeholder="ФИО" class="input">
+    <input type="text" name="to" placeholder="Е-майл" class="input">
+    <input type="text" name="theme" placeholder="Тема сообщения" class="input">
+    <textarea rows="4" type="text" name="text" placeholder="Текст сообщения" class="input"></textarea>
+    <input type="submit" name="send" text="Отправить" class="btn-reset btn">
   </form>
+  </div>
+  
 </body>
 </html>
